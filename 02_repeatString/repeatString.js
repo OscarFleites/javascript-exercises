6642f80ac3abe8994ecde069b29c681935cd400a
Oscar <fleitesoscar97@gmail.com>
2022-09-08 14:26:12 -0400

    Added logic and tested all 7 steps.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+ let i = 0;
+ let repeatedString ="";
+ if(num < 0){
+  return "ERROR"
+ }
+for (i; i < num; i++){
+  repeatedString += string;
+}
+return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
